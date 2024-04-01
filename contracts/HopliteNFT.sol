af2d6ee31ea76d5d9d1e51f85a0145cdd7073ce2
koala1029 <koala1201au@gmail.com>
2024-04-01 15:24:06 +0200

    Added the state variables to the HopliteNFT

--- contracts/HopliteNFT.sol
+++ contracts/HopliteNFT.sol
@@ -12,4 +12,46 @@ import "./IHopliteNFT.sol";
 
 
 contract HopliteNFT is Ownable, ONFT721, ERC2981, IHopliteNFT {
+    uint256 public constant MAX_SUPPLY = 5000;
+
+    string public baseTokenURI;
+
+    address public royaltyHandler;
+
+    mapping(address => bool) public whiteList;
+
+    uint256 public goLiveDate;
+
+    // Max 10% royalty
+    uint256 private constant MAX_ROYALTY = 1000;
+
+    constructor (
+        string memory baseURI,
+        uint256 _goLiveDate,
+        uint _minGasToTransfer,
+        address _lzEndpoint
+    ) ONFT721("Mozaic Hoplites", "HOP", _minGasToTransfer, _lzEndpoint) {
+        setBaseURI(baseURI);
+        goLiveDate = _goLiveDate;
+        whiteList[msg.sender] = true;
+        for(uint256 i=0; i<377; i++) {
+            _mint(msg.sender, i);
+        }
+        whiteList[msg.sender] = false;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                       Error
+    //////////////////////////////////////////////////////////////*/
+    error NotWhiteList();
+
+    /*//////////////////////////////////////////////////////////////
+                       Events
+    //////////////////////////////////////////////////////////////*/
+
+    event NewDefaultRoyalty(address receiver, uint256 newRoyalty);
+    event NewTokenRoyalty(uint256 tokenId, address receiver, uint256 newRoyalty);
+    event DeleteDefaultRoyalty();
+    event ResetTokenRoyalty(uint256 tokenId);
+    event UpdateGoLiveDate(uint256 newLiveDate);
 }
